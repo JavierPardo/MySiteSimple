b7624eca70be8fea7cc782e03d816cafdfc26688
javier pardo <javier.p.g1989@gmail.com>
2017-05-12 02:12:55 -0400

    ...

---
+++ server/api/resume/resume.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _ = require('lodash');
+var jsonfile = require('jsonfile');
+var path = require('path');
+var FILE = path.resolve('db', 'cursos.json');
+var bodyParser = require('body-parser');
