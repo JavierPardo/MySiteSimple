20b4cd45903462ee472b2cc4c03529ceb8344076
Javier Pardo Gomez <javier.p.g1989@gmail.com>
2017-05-22 12:16:11 -0400

    no message

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -6,11 +6,13 @@ var userController = require('./user.controller');
 var jsonfile = require('jsonfile');
 var path = require('path');
 var router = express.Router();
+var utils = require('../../utils').getInstance();
 
-  router.get('/authenticate', userController.authenticate);
-  router.get('/validate', userController.validate);
-  router.post('/signin', userController.signin);
-//router.put('/:id', controller.put);
+// route middleware to verify a token
+router.get('/authenticate', userController.authenticate);
+router.get('/validate', userController.validate);
+router.post('/signin', userController.signin);
+router.put('/register', userController.register);
 //router.post('/:id', controller.post);
 //router.delete('/:id', controller.delete);
 
