72ab0429d5368629846e3f49ff1b6e174e1d8473
javier pardo <javier.p.g1989@gmail.com>
2017-05-23 21:08:55 -0400

    no message

--- client/modules/commonCore/layouts/default/directives/menus/menuTopbar.ts
+++ client/modules/commonCore/layouts/default/directives/menus/menuTopbar.ts
@@ -1,3 +1,6 @@
+import { Http } from '@angular/http';
+import { ComponentType } from '../../../../models/ui/componentType';
+import { BaseComponent } from '../../../../models/ui/baseComponent';
 import { Router } from '@angular/router';
 
 import { AfterViewInit, Component, Input } from "@angular/core";
@@ -7,15 +10,19 @@ import helper from '../../../../helpers'
     selector: "menu-topbar",
     templateUrl: "./menuTopbar.html",
 })
-export class MenuTopbar implements AfterViewInit {
+export class MenuTopbar extends BaseComponent implements AfterViewInit {
     
     private router: Router;
 
-    constructor( router: Router) {        
+    constructor( router: Router, http: Http) {
+        super(http, ComponentType.Control,null)      
         this.router = router;
+        this.setResources(["topMenu"]);
     }
     ngAfterViewInit() {
         this.initializedSidebar();
+        
+        console.log(this.i18n);
     }
     /* tslint:disable */
     private initializedSidebar(): any {
--- client/modules/config/ioc.ts
+++ client/modules/config/ioc.ts
@@ -1,3 +1,4 @@
+import { ResourceHelper } from '../commonCore/helpers/resourceHelper';
 import { ApplicationState } from '../applicationState';
 import { RESTConnector } from '../commonCore/connectors/restConnector';
 import { IoCInstanceType } from '../commonCore/models/ioc/enum';
@@ -8,5 +9,7 @@ let iocRegistrations: any = [
     { name: "IEventManager", instanceOf: EventManager, type: IoCInstanceType.Singleton },
     { name: "IConnector", instanceOf: RESTConnector, type: IoCInstanceType.Singleton },
     { name: "IApplicationState", instanceOf: ApplicationState, type: IoCInstanceType.Singleton },
+    { name: "IResource", instanceOf: ResourceHelper, type: IoCInstanceType.Singleton },
+
 ];
 export default iocRegistrations;
